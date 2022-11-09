59a04eda3c3d5221c45fb3e9af083035f098d73b
Swof <ivank1401@gmail.com>
2022-11-09 16:11:03 +1300

    added main routine and comments

--- nested_image_detect.py
+++ nested_image_detect.py
@@ -24,16 +24,19 @@ if 'full.png' in images.keys():
             index += 1
             result = cv2.matchTemplate(images[image], images['full.png'], METHOD)
             
+            # we just want minimum diffrence
             mn, _, mnLoc, _ = cv2.minMaxLoc(result)
+            # extracting coordinates of best match
             MPx, MPy = mnLoc
+            # getting sizes
             trows, tcols = images[image].shape[:2]
             
+            # drawing rectangles on full.png
             cv2.rectangle(images['full.png'], (MPx, MPy), (MPx+tcols,MPy+trows),(255,0+(index*45),0+(index*22)),1)
-            cv2.imshow('output', images['full.png'])
             
-            print((MPx+(tcols/2)), (MPy+(trows/2)), image)
             absolutePos[image] = (MPx+(tcols/2)), (MPy+(trows/2))
     
+    # nose is the "center" of the face
     anchorX, anchorY = absolutePos['nose.png']
     
     for image in absolutePos.keys():
@@ -44,9 +47,16 @@ if 'full.png' in images.keys():
     jsonDict['absolutePos'] = absolutePos
     jsonDict['relativePos'] = relativePos
     
+    # write to json file so that JS script can access info
     json_obj = json.dumps(jsonDict, indent=4)
     with open(f'{IMAGEDIRECTORY}\image_data.json', 'w') as outfile:
         outfile.write(json_obj)
-    cv2.waitKey(0)
 else:
+    # reduntant
     raise KeyError('Need a full image to operate')
+
+if __name__ == '__main__':
+    # display image
+    cv2.imshow('output', images['full.png'])
+    # image only shows up if this is called
+    cv2.waitKey(0)
